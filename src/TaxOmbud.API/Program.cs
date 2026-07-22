5776c5f4d184801f48ec3f87bc4b34872419dbc6
jbochai <ochaijohnben@gmail.com>
2026-07-22 19:27:49 +0100

    chore: enable Swagger UI in all environments

--- src/TaxOmbud.API/Program.cs
+++ src/TaxOmbud.API/Program.cs
@@ -134,15 +134,15 @@ try
     app.ConfigureCustomExceptionMiddleware();
     app.UseSerilogRequestLogging();
 
-    if (app.Environment.IsDevelopment())
-    {
+    // if (app.Environment.IsDevelopment())
+    // {
         app.UseSwagger();
         app.UseSwaggerUI(c =>
         {
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tax Ombud API v1");
             c.RoutePrefix = string.Empty; // Swagger at root
         });
-    }
+    //}
 
     if (!app.Environment.IsDevelopment())
     {
